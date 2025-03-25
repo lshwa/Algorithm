9a7f95073dd672ec99f353d6bfdd2a2616589cd6
SEUNGHWA LEE <103622094+lshwa@users.noreply.github.com>
2025-03-25 17:08:22 +0900

    Update 부모의 형질을 모두 가지는 대장균 찾기.sql

--- 프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
+++ 프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
@@ -4,3 +4,9 @@ JOIN ECOLI_DATA d
     ON e.PARENT_ID = d.ID
 WHERE (e.GENOTYPE | d.GENOTYPE) = e.GENOTYPE
 ORDER BY e.ID;
+
+# 문제 풀이
+# 부모와 자식 관계를 설정하는 부분에서 자기 자신을 JOIN하는 연산이 필요해보였기에, 자기 자신과 PARENT_ID를 기준으로 JOIN 하였다.
+# 형질포함을 확인하기 위해 BITWIE OR (|) 연산을 사용해 자식 개체가 부모의 형질을 포함하는지 확인했다.
+# 결과 출력에 GENOTYPE을 PARENT_GENOTYPE으로 나오도록 별칭을 해주었고, 
+# 마지막에 GROUP BY를 사용하고, 아무것도 명시하지 않았을 때 오름차순이 나오기에 문제의 조건에 맞게 ID를 기준으로 오름차순하였다. 

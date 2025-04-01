a27d51a15cd9ba43d0406a6eefc2215216985b94
SEUNGHWA LEE <103622094+lshwa@users.noreply.github.com>
2025-04-02 08:50:02 +0900

    Update 조건별로 분류하여 주문상태 출력하기.sql

--- 프로그래머스/3/131113. 조건별로 분류하여 주문상태 출력하기/조건별로 분류하여 주문상태 출력하기.sql
+++ 프로그래머스/3/131113. 조건별로 분류하여 주문상태 출력하기/조건별로 분류하여 주문상태 출력하기.sql
@@ -7,3 +7,7 @@ SELECT ORDER_ID, PRODUCT_ID,
     END AS 출고여부
 FROM FOOD_ORDER
 ORDER BY ORDER_ID;
+
+# 먼저, 데이트 형식을 DATE_FORMAT으로 문제의 조건에 맞게 년도 월 일 이 출력되도록 함
+# CASE문을 사용해 '출고여부' 라는 컬럼을 새로 만들고, OUT_DATE 가 출고와 관련된 자료이기 때문에 이걸 활용해서 출고 미정과 완료, 대기의 케이스를 나눔.
+# 주문 ID 로 오름차순 정렬

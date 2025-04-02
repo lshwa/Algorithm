74e686cb07e12964517e75cc38887df1f8ce1118
SEUNGHWA LEE <103622094+lshwa@users.noreply.github.com>
2025-04-02 18:59:43 +0900

    Update 조건에 부합하는 중고거래 상태 조회하기.sql

--- 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -7,3 +7,7 @@ SELECT BOARD_ID, WRITER_ID, TITLE, PRICE,
 FROM USED_GOODS_BOARD
 WHERE CREATED_DATE = '2022-10-05'
 ORDER BY BOARD_ID DESC;
+
+# CASE 문을 통해 STATUS 가 SALE, RESERVED, DONE 에 맞게 각각에 대응하는 문자열이 나오도록 함. 
+# WHERE 절을 통해 2022년 10월 5일 값이 나오도록 함. 
+# DESC 에서 게시글 ID에 대해 내림차순으로 진행 

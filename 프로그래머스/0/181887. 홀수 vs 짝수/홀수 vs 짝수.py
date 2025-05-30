a0228112c0a33bd5c3b99adeb7ed594938a18b8e
SEUNGHWA LEE <103622094+lshwa@users.noreply.github.com>
2025-05-30 15:36:11 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 9.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    even_sum = 0
+    odd_sum = 0
+
+    for i in range(0, len(num_list), 2):
+        even_sum += num_list[i]  
+
+    for i in range(1, len(num_list), 2):
+        odd_sum += num_list[i]  
+
+    return max(even_sum, odd_sum)

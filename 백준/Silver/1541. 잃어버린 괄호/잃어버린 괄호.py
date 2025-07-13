b401086cd6b16469994ae57ad7028a7691a0244d
SEUNGHWA LEE <103622094+lshwa@users.noreply.github.com>
2025-07-13 12:12:13 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -0,0 +1,8 @@
+expression = input()
+parts = expression.split('-')
+result = sum(map(int, parts[0].split('+')))
+
+for part in parts[1:]:
+    result -= sum(map(int, part.split('+')))
+
+print(result)

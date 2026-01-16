ac31ef5dd8359cc67481814c51da9716b6406a1e
SEUNGHWA LEE <103622094+lshwa@users.noreply.github.com>
2026-01-16 11:32:54 +0900

    [level 0] Title: 코드 처리하기, Time: 12.20 ms, Memory: 9.36 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,23 @@
+def solution(code):
+    mode = 0
+    ret = ""
+    
+    for idx in range(len(code)):
+        if mode == 0:
+            if code[idx] == "1":
+                mode = 1
+            else: 
+                if idx % 2 == 0:
+                    ret += code[idx]
+        else:
+            if code[idx] == "1":
+                mode = 0
+            else:
+                if idx % 2 == 1:
+                    ret += code[idx]
+                    
+    return ret if ret != "" else "EMPTY"
+                    
+                
+            
+        

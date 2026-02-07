4a61ebd50b3ecc23ba623df262bbaef6348a2800
SEUNGHWA LEE <103622094+lshwa@users.noreply.github.com>
2026-02-07 13:33:25 +0900

    [Silver II] Title: 최소 힙, Time: 92 ms, Memory: 38340 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,20 @@
+import sys
+from heapq import heappush, heappop
+
+input = sys.stdin.readline
+
+N = int(input())
+heap = [] 
+out = []
+
+for _ in range(N):
+    cmd = int(input())
+    if cmd == 0:
+        if heap:
+            out.append(str(heappop(heap)))
+        else:
+            out.append('0')
+    else:
+        heappush(heap, cmd)
+
+sys.stdout.write("\n".join(out))

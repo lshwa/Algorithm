23a0de515200bbdb82621e40455924ae768fa19c
SEUNGHWA LEE <103622094+lshwa@users.noreply.github.com>
2026-02-08 15:06:15 +0900

    [Gold IV] Title: 오큰수, Time: 380 ms, Memory: 249180 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split()))
+
+result = [-1] * N
+stack = []
+
+for i in range(N):
+    while stack and A[stack[-1]] < A[i]:
+        result[stack.pop()] = A[i]
+    stack.append(i)
+
+print(' '.join(map(str, result)))

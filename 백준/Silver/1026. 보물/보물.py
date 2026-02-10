1119a48e93f5f4a702bb640963826d835f9ae790
SEUNGHWA LEE <103622094+lshwa@users.noreply.github.com>
2026-02-10 22:58:35 +0900

    [Silver IV] Title: 보물, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,12 @@
+import sys 
+input = sys.stdin.readline
+
+n = int(input().strip())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+A.sort()
+B_sorted = sorted(B, reverse=True)
+
+answer = sum(a * b for a, b in zip(A, B_sorted))
+print(answer)

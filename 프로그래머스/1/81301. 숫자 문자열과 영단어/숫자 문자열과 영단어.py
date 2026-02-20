e4d88b6123479b0fd555706cec82415135a325d6
SEUNGHWA LEE <103622094+lshwa@users.noreply.github.com>
2026-02-20 10:46:55 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 9.39 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,16 @@
+def solution(s):
+    word_dict = {
+        "zero" : "0",
+        "one" : "1", 
+        "two" : "2",
+        "three" : "3",
+        "four" : "4",
+        "five" : "5",
+        "six" : "6",
+        "seven" : "7",
+        "eight" : "8", 
+        "nine" : "9"
+    }
+    for w, d in word_dict.items():
+        s = s.replace(w, d)
+    return int(s)
